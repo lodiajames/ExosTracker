100bbbce418118a8d1d92e2f7892b2b1a0347311
ibrahima <lodiajames@yahoo.com>
2024-05-09 22:04:50 -0400

    adding build

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);

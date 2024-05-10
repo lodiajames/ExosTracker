dd2688490e421a9ace8e471df4db35b40e78e339
ibrahima <lodiajames@yahoo.com>
2024-05-09 22:07:39 -0400

    adding

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+app.use(express.static("dist"));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
